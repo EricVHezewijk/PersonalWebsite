6a0f9942f469ba2a47c5900b1fa0dbdc465bcdf2
EricVanHezewijk <ericvanhezewijk@uvic.ca>
2025-02-12 00:58:59 -0800

    worked on projects section, adding ul items and descriptions

--- personal-website/src/components/Project.jsx
+++ personal-website/src/components/Project.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Project({className, title}) {
+function Project({className, title, description, points}) {
   return (
     <div className={`project ${className}`}>
       <h3>{title}</h3>
+      <p>{description}</p>
+      <ul>
+        {points.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   )
 }
--- personal-website/src/components/ProjectContainer.jsx
+++ personal-website/src/components/ProjectContainer.jsx
@@ -6,9 +6,23 @@ function ProjectContainer() {
     <>
       <h2 className='project-header'>Projects - </h2>
       <div className='project-container'>
-        <Project className='movie-browser' title='Movie Browser' />
-        <Project className='stock-predictor' title='Machine Learning Stock Predictor'/>
-        <Project className='temp-monitor' title='Temperature Monitoring System'/>
+        <Project  className='movie-browser' 
+                  title='Movie Browser' 
+                  description='Created a movie browing website in React, allowing users to browse, search and favourite movies that are fetched from a RESTful API.'
+                  points={[]}
+        />
+
+        <Project  className='stock-predictor' 
+                  title='Machine Learning Stock Predictor' 
+                  description='Machine learning web app to predict daily stock price movement using pands, numpy and scikit-learn to clean and analyze data.'
+                  points={[]}
+        />
+
+        <Project  className='temp-monitor' 
+                  title='Temperature Monitoring System' 
+                  description='IoT-based temperature monitoring system, designed with Pico W microcontroller controlled by Python and relayed to a web based UI.'
+                  points={[]}
+        />
       </div> 
     </>
     
