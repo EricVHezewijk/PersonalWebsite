bc184ffe0513640e0d4615b974a3a10152870a1a
EricVanHezewijk <ericvanhezewijk@uvic.ca>
2025-02-24 23:11:47 -0800

    udated icon positioning since they were clipping on large screens

--- personal-website/src/components/AboutMe.jsx
+++ personal-website/src/components/AboutMe.jsx
@@ -13,6 +13,14 @@ function AboutMe() {
             <br />
             I now apply these skills to my programming, hoping to create beautiful and innovative software. In fact, my passion for programming may never have been sparked if it weren't for my interest in cars — which led me into the engineering field in the first place.
           </p>
+          <div className="icon-container">
+            <div className="engine-icon">
+              <img src="engine.png" />
+            </div>
+            <div className="piston-icon">
+              <img src="piston.png" />
+            </div>
+          </div>
         </div >
         <div className="image-gallery">
           <div className="landscape-img-container">
@@ -23,15 +31,6 @@ function AboutMe() {
             <img src="car3.jpeg" />
           </div>
         </div>
-        <div className="icon-container">
-          <div className="engine-icon">
-            <img src="engine.png" />
-          </div>
-          <div className="piston-icon">
-            <img src="piston.png" />
-          </div>
-        </div>
-        
       </div>
 
       <div className="interest-container">
@@ -50,16 +49,13 @@ function AboutMe() {
           <p className="interest-description">
             Growing up on Vancouver Island has given me an incredible opportunity to immerse myself in the stunning natural environment that surrounds me. From my early elementary school field trips, I developed a deep appreciation for the breathtaking landscapes and vibrant ecosystems of this beautiful part of the world. I’m passionate about a variety of outdoor activities, including camping, hiking, trail running, and swimming in the pristine lakes and oceans that BC is known for. While I’m fortunate to live in a province filled with endless outdoor adventures, I also have a strong love for traveling and discovering new corners of the world. Every new destination offers a fresh perspective on nature, and I relish the opportunity to connect with the natural beauty of different places.
           </p>
-        </div>
-        <div className="icon-container">
-          <div className="tree-icon">
-            <img src="tree.png" />
-          </div>
-          <div className="mountain-icon">
-            <img src="mountain.png" />
+          <div className="icon-container">
+            <div className="mountain-icon">
+              <img src="mountain.png" />
+            </div>
           </div>
         </div>
-
+        
       </div>
 
       <div className="interest-container">
