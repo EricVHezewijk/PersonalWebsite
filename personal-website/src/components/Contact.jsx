d8b9f8b7861ea5409db5ece0172796222dafc037
EricVanHezewijk <ericvanhezewijk@uvic.ca>
2025-03-27 21:27:24 -0700

    Added contact section at end

--- personal-website/src/components/Contact.jsx
+++ personal-website/src/components/Contact.jsx
@@ -4,7 +4,7 @@ function Contact() {
   return (
     <div className="contact-wrapper">
       <div className="contact-container">
-        <h2 className='contact-header'>Contact Me</h2>
+        <h2 className='contact-header'>Get in touch</h2>
         <div className="location-container">
             <i class="fa-solid fa-location-dot"></i>
             <h4>Victoria, BC</h4>
@@ -12,19 +12,18 @@ function Contact() {
         <div className="contact-icons-container">
           <div className="phone">
             <i class="fa-solid fa-phone"></i>
-            <h3>778-587-6205</h3>
           </div>
           <div className="email">
             <i class="fa-solid fa-envelope"></i>
-            <h3>ericvanhezewijk7@gmail.com</h3>
           </div>
-        </div>
-        <div className="github">
+          <div className="github">
             <i class="fa-brands fa-github"></i>
           </div>
           <div className="linkedin">
             <i class="fa-brands fa-linkedin"></i>
           </div>
+        </div>
+        
       </div>
     </div>
   )
