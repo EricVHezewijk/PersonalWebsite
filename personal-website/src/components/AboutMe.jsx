87257804ca82790eacdc61a56666d3eccdddeb81
Eric Van Hezewijk <ericvanhezewijk@uvic.ca>
2026-04-18 16:22:58 -0700

    Small css tweak and reworded about me section

--- personal-website/src/components/AboutMe.jsx
+++ personal-website/src/components/AboutMe.jsx
@@ -11,7 +11,7 @@ function AboutMe() {
             When I'm not programming, I love working on cars. From a young age, I've been intrigued by the complexity of engines. Over the years, I've spent countless hours working on both the mechanical and visual aspects of my car. One of my biggest projects was removing and rebuilding the engine, which required constant problem-solving and perseverance. On the visual side, vinyl wrapping my car required careful attention to detail, ensuring every measurement, line, and curve was flawless. 
             <br />
             <br />
-            I now apply these skills to my programming, hoping to create beautiful, thoughtful software. In fact, I may have never discovered my passion for programming if it weren't for my interest in cars, as that led me into the engineering field in the first place.
+            I now apply these skills to my programming to create clean, thoughtful software. In fact, I may have never discovered my passion for programming if it weren't for my interest in cars, as that led me into the engineering field in the first place.
           </p>
           <div className="icon-container">
             <div className="engine-icon">
@@ -47,10 +47,10 @@ function AboutMe() {
         <div className="interest-text nature">
           <h2 className="interest-title">Outdoor Ventures</h2>
           <p className="interest-description">
-            Growing up on Vancouver Island has given me an incredible opportunity to immerse myself in the stunning environment that surrounds me. From a young age, I developed a great appreciation for the beautiful landscapes and ecosystems that surround me. I'm passionate about a variety of outdoor activities, including camping, hiking, trail running, and swimming in the chilly lakes and oceans that BC is known for. Some of my most recent ventures this past summer include summiting the 5040 Peak and hiking the coastal Juan de Fuca Trail!
+            Growing up on Vancouver Island has given me the opportunity to experience the amazing island's scenery. I'm passionate about a variety of outdoor activities, including camping, hiking, trail running, and swimming in the chilly lakes and oceans that BC is known for. Some of my most recent ventures include summiting the 5040 Peak and hiking the coastal Juan de Fuca Trail!
             <br/>
             <br/>
-            While I'm fortunate to live in a province filled with endless outdoor adventures, I also have a strong desire to travel and discover new cultures around the world. Some of my bucket list locations are Switzerland (especially the Alps), Italy, Southeast Asia and Japan.
+            While I'm fortunate to live in a such a beautiful province, I also have a strong desire to travel and discover new cultures around the world. Some of my bucket list locations are Switzerland (particularily the Alps), Italy, Southeast Asia and Japan.
           </p>
           <div className="icon-container">
             <div className="mountain-icon">
